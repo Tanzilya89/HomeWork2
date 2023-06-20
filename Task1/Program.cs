d43ed321f374d625a6ea8a65500470ac35d6c8af
Tanzilya Tenitskaya <taya.taya.1989@mail.ru>
2023-06-20 12:37:19 +0300

    HomeWork2

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число
+//и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трехзначное число ");
+int Value = Convert.ToInt32(Console.ReadLine());
+int Length = Value.ToString().Length;
+
+if (Length == 3)
+{
+    int result = (Value / 10) % 10;
+    Console.Write($"Вторая цифра - {result}");
+}
+else
+{
+    Console.Write("Это не трехзначное число ");
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+//или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число");
+int Value2 = Convert.ToInt32(Console.ReadLine());
+int Length2 = Value2.ToString().Length;
+
+if (Length2 >= 3)
+{
+    while (Value2 > 999)
+    {
+        Value2 = Value2 / 10;
+    }
+    int result = Value2 % 10;
+    Console.WriteLine($"Третья цифра - {result}");
+}
+else
+{
+    Console.WriteLine("Третья цифра отсутствует");
+}
